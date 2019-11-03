aa540bb1315c3d48d5ce685c8509437b876f62a8
MichalGuz <michalguz@gmail.com>
2019-11-04 00:33:53 +0100

    Constructor in class DbManager throws SQLException.

--- src/main/java/com/spring/jdbchibernate/jdbc/DbManager.java
+++ src/main/java/com/spring/jdbchibernate/jdbc/DbManager.java
@@ -1,10 +1,11 @@
 package com.spring.jdbchibernate.jdbc;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DbManager {
     private Connection connection;
     private static DbManager dbManagerInstance;
 
-    private DbManager(){}
+    private DbManager() throws SQLException {}
 }
