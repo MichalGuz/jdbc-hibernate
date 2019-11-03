ba80a3557c4da51a64f30692ab290af0458df25d
MichalGuz <michalguz@gmail.com>
2019-11-04 00:23:11 +0100

    first commit

---
+++ src/main/java/com/spring/jdbchibernate/JdbcHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jdbchibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jdbchibernate/JdbcHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jdbchibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
