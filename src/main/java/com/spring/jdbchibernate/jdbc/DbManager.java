b5ee5f74b17cc9ddf1884328d97cf9daf3b13674
MichalGuz <michalguz@gmail.com>
2019-11-04 22:37:29 +0100

    Create new Properties object inside constructor DbManager.

--- src/main/java/com/spring/jdbchibernate/jdbc/DbManager.java
+++ src/main/java/com/spring/jdbchibernate/jdbc/DbManager.java
@@ -2,10 +2,13 @@ package com.spring.jdbchibernate.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbManager {
     private Connection connection;
     private static DbManager dbManagerInstance;
 
-    private DbManager() throws SQLException {}
+    private DbManager() throws SQLException {
+        Properties connectionProperties = new Properties();
+    }
 }
