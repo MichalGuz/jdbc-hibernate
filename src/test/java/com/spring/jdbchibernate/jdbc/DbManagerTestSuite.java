4fb3cf8a253fd3b1144fe123de52c26656b31c9a
MichalGuz <michalguz@gmail.com>
2019-11-05 00:10:23 +0100

    Create empty 'given' section of  testConnect() and the 'when' section of test cointaining getInstance()invocation.

--- src/test/java/com/spring/jdbchibernate/jdbc/DbManagerTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/jdbc/DbManagerTestSuite.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 public class DbManagerTestSuite {
     @Test
     public void testConnect() {
+        // given
 
+        // when
+        DbManager dbManager = DbManager.getInstance();
     }
 
 }
