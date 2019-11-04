0d2e449d665e4b5ec8c9d931a809f734ced34274
MichalGuz <michalguz@gmail.com>
2019-11-05 00:11:10 +0100

    Method testConnect() throws SQLException.

--- src/test/java/com/spring/jdbchibernate/jdbc/DbManagerTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/jdbc/DbManagerTestSuite.java
@@ -2,9 +2,11 @@ package com.spring.jdbchibernate.jdbc;
 
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 public class DbManagerTestSuite {
     @Test
-    public void testConnect() {
+    public void testConnect() throws SQLException {
         // given
 
         // when
