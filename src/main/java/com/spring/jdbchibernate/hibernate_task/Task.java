0ec7053d3bf77167703abbc192b6f2d06e1b9718
MichalGuz <michalguz@gmail.com>
2019-11-09 23:59:21 +0100

    Create four fields of class Task.

--- src/main/java/com/spring/jdbchibernate/hibernate_task/Task.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_task/Task.java
@@ -1,4 +1,10 @@
 package com.spring.jdbchibernate.hibernate_task;
 
+import java.util.Date;
+
 public class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
 }
