0ee4a14b3eaeed06d42c4b9af2c7dcdb64cb28f5
MichalGuz <michalguz@gmail.com>
2019-11-10 23:49:42 +0100

    Intreface TaskDao extends CrudRepository.

--- src/main/java/com/spring/jdbchibernate/hibenate_task/dao/TaskDao.java
+++ src/main/java/com/spring/jdbchibernate/hibenate_task/dao/TaskDao.java
@@ -1,4 +1,7 @@
 package com.spring.jdbchibernate.hibenate_task.dao;
 
-public interface TaskDao {
+import com.spring.jdbchibernate.hibernate_task.Task;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TaskDao extends CrudRepository<Task, Integer> {
 }
