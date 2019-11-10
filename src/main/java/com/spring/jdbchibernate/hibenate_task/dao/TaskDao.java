569fed1e6fbf19557ae9d2e5651d24795000aafc
MichalGuz <michalguz@gmail.com>
2019-11-10 23:55:26 +0100

    Create method findByDuration() inside class TaskDao.

--- src/main/java/com/spring/jdbchibernate/hibenate_task/dao/TaskDao.java
+++ src/main/java/com/spring/jdbchibernate/hibenate_task/dao/TaskDao.java
@@ -3,5 +3,8 @@ package com.spring.jdbchibernate.hibenate_task.dao;
 import com.spring.jdbchibernate.hibernate_task.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TaskDao extends CrudRepository<Task, Integer> {
+    List<Task> findByDuration(int duration);
 }
