def03469c316e4683324ce91e468df2dd00a4b8e
MichalGuz <michalguz@gmail.com>
2019-11-11 23:50:45 +0100

    Create field TaskDao taskDao in TaskDaoTestSuite class.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
@@ -1,8 +1,10 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
+import com.spring.jdbchibernate.hibenate_task.dao.TaskDao;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TaskDaoTestSuite {
+    private TaskDao taskDao;
 }
