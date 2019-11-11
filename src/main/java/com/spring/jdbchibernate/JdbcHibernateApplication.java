269506346395df24dece3e66c051904ab69c0d14
MichalGuz <michalguz@gmail.com>
2019-11-11 23:40:56 +0100

    Restore the previous name of class  SpringRunne: back to name JdbcHibernateApplication.

--- src/main/java/com/spring/jdbchibernate/JdbcHibernateApplication.java
+++ src/main/java/com/spring/jdbchibernate/JdbcHibernateApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRunner {
+public class JdbcHibernateApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRunner.class, args);
+		SpringApplication.run(JdbcHibernateApplication.class, args);
 	}
 }
 
--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
@@ -1,5 +1,4 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
-
 public class TaskDaoTestSuite {
 }
