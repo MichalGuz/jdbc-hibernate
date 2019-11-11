cdb4815e44e42b3a636109c4f855b8f7a49b4f79
MichalGuz <michalguz@gmail.com>
2019-11-11 23:51:28 +0100

    Add annotation @SpringBootTest to TaskDaoTestSuite class.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
@@ -2,9 +2,11 @@ package com.spring.jdbchibernate.hibernate_task.dao;
 
 import com.spring.jdbchibernate.hibenate_task.dao.TaskDao;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskDaoTestSuite {
     private TaskDao taskDao;
 }
