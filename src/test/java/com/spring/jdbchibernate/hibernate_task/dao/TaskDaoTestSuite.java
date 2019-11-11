0235e5615fef035e4578355fa7b96d12bdd6e4cc
MichalGuz <michalguz@gmail.com>
2019-11-11 23:54:57 +0100

    Create method testTaskDaoSave() in TaskDaoTestSuite class.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
@@ -1,6 +1,7 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
 import com.spring.jdbchibernate.hibenate_task.dao.TaskDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,4 +11,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TaskDaoTestSuite {
     private TaskDao taskDao;
     private static final String DESCRIPTION = "Test: practise Hibernate.";
+
+    @Test
+    public void testTaskDaoSave() {
+
+    }
 }
