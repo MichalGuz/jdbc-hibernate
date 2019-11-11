db3f48636e35f48b82a98d1b2692653aeca7ad73
MichalGuz <michalguz@gmail.com>
2019-11-11 23:47:55 +0100

    Add annotation @RunWith(SpringRunner.class) to TaskDaoTestSuite class.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskDaoTestSuite.java
@@ -1,4 +1,8 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class TaskDaoTestSuite {
 }
