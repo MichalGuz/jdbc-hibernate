6c411cae3916da93c23b26000a712583a9e835c7
MichalGuz <michalguz@gmail.com>
2019-11-12 22:01:09 +0100

    Add an annotation @Repository to class TaskDao (for creation of Bean - the controller of repository).

--- src/main/java/com/spring/jdbchibernate/hibenate_task/dao/TaskDao.java
+++ src/main/java/com/spring/jdbchibernate/hibenate_task/dao/TaskDao.java
@@ -2,11 +2,13 @@ package com.spring.jdbchibernate.hibenate_task.dao;
 
 import com.spring.jdbchibernate.hibernate_task.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
+@Repository
 public interface TaskDao extends CrudRepository<Task, Integer> {
     List<Task> findByDuration(int duration);
 }
