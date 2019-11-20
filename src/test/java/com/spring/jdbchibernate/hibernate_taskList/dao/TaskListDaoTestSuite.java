0990b0d11182af6ce2e562c273906b3a36581b75
MichalGuz <michalguz@gmail.com>
2019-11-20 22:39:46 +0100

    Create 'given' section of class TaskListDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_taskList/dao/TaskListDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_taskList/dao/TaskListDaoTestSuite.java
@@ -1,10 +1,12 @@
 package com.spring.jdbchibernate.hibernate_taskList.dao;
 
+import com.spring.jdbchibernate.hibernate_tasklist.TaskList;
 import org.junit.Test;
 
 public class TaskListDaoTestSuite {
     @Test
     public void testFindByListName() {
-
+        // given
+        TaskList taskList = new TaskList();
     }
 }
