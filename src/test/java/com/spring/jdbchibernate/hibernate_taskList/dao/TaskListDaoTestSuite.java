7e2c545f2bcae7c046c81da6554b1de22338dd19
MichalGuz <michalguz@gmail.com>
2019-11-20 22:48:09 +0100

    Create TaskListDao and static field DESCRIPTION.

--- src/test/java/com/spring/jdbchibernate/hibernate_taskList/dao/TaskListDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_taskList/dao/TaskListDaoTestSuite.java
@@ -1,12 +1,18 @@
 package com.spring.jdbchibernate.hibernate_taskList.dao;
 
 import com.spring.jdbchibernate.hibernate_tasklist.TaskList;
+import com.spring.jdbchibernate.hibernate_tasklist.dao.TaskListDao;
 import org.junit.Test;
 
 public class TaskListDaoTestSuite {
+
+    private TaskListDao taskListDao;
+    private static final String DECSRIPTION = "Description for test.";
+
     @Test
     public void testFindByListName() {
         // given
         TaskList taskList = new TaskList();
+
     }
 }
