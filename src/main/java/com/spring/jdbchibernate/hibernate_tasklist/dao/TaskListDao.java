252e34061564cdb81f58d097418f9aa4299e3648
MichalGuz <michalguz@gmail.com>
2019-11-20 22:19:44 +0100

    Interface TaskListDao extends CrudeRepository.

--- src/main/java/com/spring/jdbchibernate/hibernate_tasklist/dao/TaskListDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_tasklist/dao/TaskListDao.java
@@ -1,4 +1,10 @@
 package com.spring.jdbchibernate.hibernate_tasklist.dao;
 
-public class TaskListDao {
+import com.spring.jdbchibernate.hibernate_tasklist.TaskList;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TaskListDao extends CrudRepository {
+
 }
