6d2b9aaab1aa9bfa033392e44af33859061aa743
MichalGuz <michalguz@gmail.com>
2019-11-21 23:01:42 +0100

    Create fields in class Project.

---
+++ src/main/java/com/spring/jdbchibernate/hibernate_project/Project.java
@@ -0,0 +1,13 @@
+package com.spring.jdbchibernate.hibernate_project;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Project {
+    private int id;
+    private String title;
+    private BigDecimal worthOfProject;
+    private BigDecimal worthOfGrant;
+    private String fund;
+    private Date applied;
+}
