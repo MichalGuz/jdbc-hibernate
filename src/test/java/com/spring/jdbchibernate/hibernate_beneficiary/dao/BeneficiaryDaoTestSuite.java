f2f4d881a531395ea43bcf1f3b66d459abfc225a
MichalGuz <michalguz@gmail.com>
2019-11-21 22:30:42 +0100

    Create testFindByEditorKey() method in class BeneficiaryDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
@@ -1,5 +1,6 @@
 package com.spring.jdbchibernate.hibernate_beneficiary.dao;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,5 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class BeneficiaryDaoTestSuite {
+    @Test
+    public void testFindByEditorKey() {
+
+    }
 
 }
