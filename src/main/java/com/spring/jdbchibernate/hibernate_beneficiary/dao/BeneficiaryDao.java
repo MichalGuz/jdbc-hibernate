f3dc24f2e0b6a7bc5ee9c599b323a3d954fc2d43
MichalGuz <michalguz@gmail.com>
2019-11-21 22:16:15 +0100

    Create findByEditorKey() method in interface BeneficiaryDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
@@ -3,6 +3,8 @@ package com.spring.jdbchibernate.hibernate_beneficiary.dao;
 import com.spring.jdbchibernate.hibernate_beneficiary.Beneficiary;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BeneficiaryDao extends CrudRepository<Beneficiary, Integer> {
+import java.util.List;
 
+public interface BeneficiaryDao extends CrudRepository<Beneficiary, Integer> {
+    public List<Beneficiary> findByEditorKey(int editorKey);
 }
