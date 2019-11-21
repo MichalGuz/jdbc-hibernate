010f2520955ee99f60a16a57d9c65a372945743e
MichalGuz <michalguz@gmail.com>
2019-11-21 22:53:02 +0100

    Add annotation @Autowired to field beneficiaryDao in BeneficiaryDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
@@ -4,6 +4,7 @@ import com.spring.jdbchibernate.hibernate_beneficiary.Beneficiary;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 public class BeneficiaryDaoTestSuite {
 
+    @Autowired
     BeneficiaryDao beneficiaryDao;
 
     @Test
