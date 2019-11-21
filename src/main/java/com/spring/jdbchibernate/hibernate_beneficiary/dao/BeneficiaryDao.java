c62364c2d4a7b7b993d8c2e1edb9398b8d195499
MichalGuz <michalguz@gmail.com>
2019-11-21 22:17:56 +0100

    Add annotations @Transactional and @Repository to interface BeneficiaryDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
@@ -2,9 +2,13 @@ package com.spring.jdbchibernate.hibernate_beneficiary.dao;
 
 import com.spring.jdbchibernate.hibernate_beneficiary.Beneficiary;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
+@Repository
 public interface BeneficiaryDao extends CrudRepository<Beneficiary, Integer> {
     public List<Beneficiary> findByEditorKey(int editorKey);
 }
