daff4aaf2857bed4a99ae79b6d9a88c564eee03f
MichalGuz <michalguz@gmail.com>
2019-11-21 21:38:05 +0100

    Create constructor of class Beneficiary.

--- src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/Beneficiary.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/Beneficiary.java
@@ -4,4 +4,9 @@ public class Beneficiary {
     private int id;
     private String name;
     private int editorKey;
+
+    public Beneficiary(String name, int editorKey) {
+        this.name = name;
+        this.editorKey = editorKey;
+    }
 }
