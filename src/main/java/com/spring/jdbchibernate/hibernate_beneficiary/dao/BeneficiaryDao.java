69a29ce643a225cada30d2c5b894d9f64e9320ce
MichalGuz <michalguz@gmail.com>
2019-11-21 22:11:29 +0100

    Interface BeneficiaryDao extends CrudRepository.

--- src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
@@ -4,4 +4,5 @@ import com.spring.jdbchibernate.hibernate_beneficiary.Beneficiary;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BeneficiaryDao extends CrudRepository<Beneficiary, Integer> {
+
 }
