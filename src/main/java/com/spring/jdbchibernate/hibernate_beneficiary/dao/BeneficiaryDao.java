fd16c5eff735ca3aa7cbfab0dcc1987181ff784a
MichalGuz <michalguz@gmail.com>
2019-11-21 22:08:43 +0100

    Create package 'hibernate_beneficiary.dao' and interface BeneficiaryDao.

---
+++ src/main/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDao.java
@@ -0,0 +1,7 @@
+package com.spring.jdbchibernate.hibernate_beneficiary.dao;
+
+import com.spring.jdbchibernate.hibernate_beneficiary.Beneficiary;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BeneficiaryDao extends CrudRepository<Beneficiary, Integer> {
+}
