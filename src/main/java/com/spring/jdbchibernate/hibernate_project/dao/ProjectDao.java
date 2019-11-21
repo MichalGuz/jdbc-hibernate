1c7660eec034b2f102bd1b4678c2bc9626794620
MichalGuz <michalguz@gmail.com>
2019-11-21 23:39:16 +0100

    Add annotation @Transactional and @Repository to ProjectDao interface.

--- src/main/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectDao.java
@@ -2,7 +2,12 @@ package com.spring.jdbchibernate.hibernate_project.dao;
 
 import com.spring.jdbchibernate.hibernate_project.Project;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface ProjectDao extends CrudRepository<Project, Integer> {
     Project findByTitleAndChecksum(String title, String checksum);
 }
