0b979de048908d606232ee8c381f434b59927f27
MichalGuz <michalguz@gmail.com>
2019-11-21 22:37:16 +0100

    Declare field BenficiaryDao and 'given' section in testFindByEditorKey() method.

--- src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
@@ -1,5 +1,6 @@
 package com.spring.jdbchibernate.hibernate_beneficiary.dao;
 
+import com.spring.jdbchibernate.hibernate_beneficiary.Beneficiary;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +9,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class BeneficiaryDaoTestSuite {
+
+    BeneficiaryDao beneficiaryDao;
+
     @Test
     public void testFindByEditorKey() {
+        // given
+        Beneficiary beneficiary = new Beneficiary("Funds Advisors LTD", 12341234);
 
     }
 
