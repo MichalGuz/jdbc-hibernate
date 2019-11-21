eeebb27fbbfc6eae5b8c16946a585907948a0711
MichalGuz <michalguz@gmail.com>
2019-11-21 22:27:35 +0100

    Add annotations @SpringBootTest and @RunWith to class BeneficiaryDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_beneficiary/dao/BeneficiaryDaoTestSuite.java
@@ -1,4 +1,11 @@
 package com.spring.jdbchibernate.hibernate_beneficiary.dao;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class BeneficiaryDaoTestSuite {
+
 }
