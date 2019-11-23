5ff3111c196fd4597027275f606c4931915aec9b
MichalGuz <michalguz@gmail.com>
2019-11-23 17:49:52 +0100

    Create fields of class TaskFinancialDetails.

--- src/main/java/com/spring/jdbchibernate/hibernate_task/TaskFinancialDetails.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_task/TaskFinancialDetails.java
@@ -1,4 +1,9 @@
 package com.spring.jdbchibernate.hibernate_task;
 
+import java.math.BigDecimal;
+
 public class TaskFinancialDetails {
+    private int id;
+    private BigDecimal price;
+    private boolean paid;
 }
