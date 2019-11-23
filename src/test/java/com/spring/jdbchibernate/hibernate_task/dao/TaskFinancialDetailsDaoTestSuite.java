e91810c0035db89a395a1c9695e7c81610b7913a
MichalGuz <michalguz@gmail.com>
2019-11-23 18:38:22 +0100

    Add annotations @SpringBootTest and @RunWith(Spring|Runner.class) to the class TaskFinancialDetailsDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinancialDetailsDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinancialDetailsDaoTestSuite.java
@@ -1,4 +1,10 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TaskFinancialDetailsDaoTestSuite {
 }
