d68db85d7b140859fb6f5708ca69d6be32385266
MichalGuz <michalguz@gmail.com>
2019-11-23 18:31:07 +0100

    Add annotations @Transactional and @Repository to the interface TaskFinacialDetailsDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinacialDetailsDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinacialDetailsDao.java
@@ -2,9 +2,13 @@ package com.spring.jdbchibernate.hibernate_task.dao;
 
 import com.spring.jdbchibernate.hibernate_task.TaskFinancialDetails;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
+@Repository
 public interface TaskFinacialDetailsDao extends CrudRepository<TaskFinancialDetails, Integer> {
     List<TaskFinancialDetails> findByPaid(boolean paid);
 }
