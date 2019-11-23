f8e8d7fbef318ac442f27189779a86dc0b9057bc
MichalGuz <michalguz@gmail.com>
2019-11-23 18:26:53 +0100

    Interface TaskFinacialDetailsDao extends CrudRepository<TaskFinancialDetails, Integer>

--- src/main/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinacialDetailsDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinacialDetailsDao.java
@@ -1,5 +1,8 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
-public interface TaskFinacialDetailsDao {
+import com.spring.jdbchibernate.hibernate_task.TaskFinancialDetails;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TaskFinacialDetailsDao extends CrudRepository<TaskFinancialDetails, Integer> {
 
 }
