bc0f9dd0a3386e87c046c8333c677f78499a3d1a
MichalGuz <michalguz@gmail.com>
2019-11-23 18:59:56 +0100

    Create 'given' section of testFindByPaid() and new object of TaskFinancialDetails class. Amend BigDecimal values in test method of class ProjectDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinancialDetailsDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinancialDetailsDaoTestSuite.java
@@ -1,9 +1,12 @@
 package com.spring.jdbchibernate.hibernate_task.dao;
 
+import com.spring.jdbchibernate.hibernate_task.TaskFinancialDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -12,6 +15,7 @@ public class TaskFinancialDetailsDaoTestSuite {
 
     @Test
     public void testFindByPaid() {
-
+        // given
+        TaskFinancialDetails taskFinancialDetails = new TaskFinancialDetails(new BigDecimal(115), false);
     }
 }
