912c50fa80d2812470721fee81351567d1b831cc
MichalGuz <michalguz@gmail.com>
2019-11-23 19:44:25 +0100

    Add annotation @Autowired to field taskFinacialDetailsDao in TaskFinancialDetailsDaoTestSuite class.

--- src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinancialDetailsDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_task/dao/TaskFinancialDetailsDaoTestSuite.java
@@ -4,6 +4,7 @@ import com.spring.jdbchibernate.hibernate_task.TaskFinancialDetails;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TaskFinancialDetailsDaoTestSuite {
+    @Autowired
     TaskFinacialDetailsDao taskFinacialDetailsDao;
 
     @Test
