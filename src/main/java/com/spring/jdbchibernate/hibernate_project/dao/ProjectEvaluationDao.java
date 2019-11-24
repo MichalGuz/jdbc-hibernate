d5fa7ecd98e92cc2932609fb01b387f5b7680f22
MichalGuz <michalguz@gmail.com>
2019-11-24 23:51:21 +0100

    Create method findByPositive() method of interface ProjectEvaluationDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectEvaluationDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectEvaluationDao.java
@@ -3,5 +3,8 @@ package com.spring.jdbchibernate.hibernate_project.dao;
 import com.spring.jdbchibernate.hibernate_project.ProjectEvaluation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectEvaluationDao extends CrudRepository<ProjectEvaluation, Integer> {
+    List<ProjectEvaluation> findByPositive(boolean positive);
 }
