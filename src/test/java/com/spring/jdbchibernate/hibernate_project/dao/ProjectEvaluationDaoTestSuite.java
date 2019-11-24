ea6eb212f2faf1b6c3af4d65ea2200e1eb45e130
MichalGuz <michalguz@gmail.com>
2019-11-25 00:14:19 +0100

    Add annotation @Autowired to ProjectEvaluationDao field in ProjectEvaluationDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectEvaluationDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectEvaluationDaoTestSuite.java
@@ -4,6 +4,7 @@ import com.spring.jdbchibernate.hibernate_project.ProjectEvaluation;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ProjectEvaluationDaoTestSuite {
+    @Autowired
     ProjectEvaluationDao projectEvaluationDao;
 
     @Test
