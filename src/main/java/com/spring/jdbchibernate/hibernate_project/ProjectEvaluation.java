d4243a174c6797781cc6981ffe345124c8da3602
MichalGuz <michalguz@gmail.com>
2019-11-24 14:13:12 +0100

    Add int id  atribute of ProjectEvaluation class.

--- src/main/java/com/spring/jdbchibernate/hibernate_project/ProjectEvaluation.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_project/ProjectEvaluation.java
@@ -1,10 +1,13 @@
 package com.spring.jdbchibernate.hibernate_project;
 
 public class ProjectEvaluation {
+    private int id;
     private boolean formal;
     private boolean meritoric;
     private int meritScore;
     private boolean financialAnalysis;
     private int finAnaScore;
     private boolean positive;
+
+
 }
