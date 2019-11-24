e8dc4009101d4f83c9f69cc6d0c25e53acd2ab33
MichalGuz <michalguz@gmail.com>
2019-11-24 14:11:18 +0100

    Create atributes of ProjectEvaluation class.

--- src/main/java/com/spring/jdbchibernate/hibernate_project/ProjectEvaluation.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_project/ProjectEvaluation.java
@@ -1,4 +1,10 @@
 package com.spring.jdbchibernate.hibernate_project;
 
 public class ProjectEvaluation {
+    private boolean formal;
+    private boolean meritoric;
+    private int meritScore;
+    private boolean financialAnalysis;
+    private int finAnaScore;
+    private boolean positive;
 }
