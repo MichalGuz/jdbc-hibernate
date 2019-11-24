abbfad5870a43307a4b1faef0e8ee548cb709935
MichalGuz <michalguz@gmail.com>
2019-11-24 23:53:09 +0100

    Add annotations @Repository and @Transactional to the interface ProjectEvaluationDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectEvaluationDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectEvaluationDao.java
@@ -2,9 +2,13 @@ package com.spring.jdbchibernate.hibernate_project.dao;
 
 import com.spring.jdbchibernate.hibernate_project.ProjectEvaluation;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
+@Repository
 public interface ProjectEvaluationDao extends CrudRepository<ProjectEvaluation, Integer> {
     List<ProjectEvaluation> findByPositive(boolean positive);
 }
