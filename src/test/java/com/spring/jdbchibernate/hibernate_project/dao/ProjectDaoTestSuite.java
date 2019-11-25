66651d4274719ad7fef58f03c9ffcacecff3224d
MichalGuz <michalguz@gmail.com>
2019-11-25 22:09:53 +0100

    Delete useless import.

--- src/test/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_project/dao/ProjectDaoTestSuite.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
-import java.nio.file.Path;
 import java.util.List;
 
 @SpringBootTest
