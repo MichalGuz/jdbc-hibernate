d925d52abcc4c127a5d7651420619733038a1d60
MichalGuz <michalguz@gmail.com>
2019-11-26 23:53:42 +0100

    Add annotations @Transactional and @Repository to interface SubscriptionDao.'

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDao.java
@@ -1,6 +1,11 @@
 package com.spring.jdbchibernate.hibernate_mtm_subscription.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface SubscriptionDao extends CrudRepository {
 }
