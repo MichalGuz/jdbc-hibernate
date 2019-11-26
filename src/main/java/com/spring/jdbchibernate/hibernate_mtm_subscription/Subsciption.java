0bcbcb661704b4c048814f106ffb8d691d6b5c8c
MichalGuz <michalguz@gmail.com>
2019-11-26 23:29:49 +0100

    Create fields of class Subscription and constructors (with and without parameters).

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/Subsciption.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/Subsciption.java
@@ -1,4 +1,13 @@
 package com.spring.jdbchibernate.hibernate_mtm_subscription;
 
 public class Subsciption {
+    private int id;
+    private String name;
+
+    public Subsciption(String name) {
+        this.name = name;
+    }
+
+    public Subsciption() {
+    }
 }
