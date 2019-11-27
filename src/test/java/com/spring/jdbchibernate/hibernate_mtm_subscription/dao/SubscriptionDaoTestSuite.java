1a1a19d88bf93eb6f59b9184094cb5815c0172f3
MichalGuz <michalguz@gmail.com>
2019-11-27 23:46:52 +0100

    Create testSaveManyToMany() in SubscriptionDaoTestSuite class.

--- src/test/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDaoTestSuite.java
@@ -1,5 +1,6 @@
 package com.spring.jdbchibernate.hibernate_mtm_subscription.dao;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,4 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SubscriptionDaoTestSuite {
     SubscriptionDao subscriptionDao;
+
+    @Test
+    public void testSaveManyToMany() {
+
+    }
 }
