130cb3202b205621d8f8477b70a796a885c0deb0
MichalGuz <michalguz@gmail.com>
2019-11-27 23:44:57 +0100

    Add annotations @RunWith and @SpringBootTest to the class SubscriptionDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDaoTestSuite.java
@@ -1,4 +1,11 @@
 package com.spring.jdbchibernate.hibernate_mtm_subscription.dao;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SubscriptionDaoTestSuite {
+
 }
