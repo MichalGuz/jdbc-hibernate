e3c5908b6c5a3cecb386ac6860e2b28e94f15854
MichalGuz <michalguz@gmail.com>
2019-11-28 23:11:46 +0100

    Create two constructors of class Product: with and without parameters.

--- src/main/java/com/spring/jdbchibernate/hibernate_invoice/Product.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_invoice/Product.java
@@ -3,4 +3,13 @@ package com.spring.jdbchibernate.hibernate_invoice;
 public class Product {
     private int id;
     private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Product() {
+    }
 }
+
+
