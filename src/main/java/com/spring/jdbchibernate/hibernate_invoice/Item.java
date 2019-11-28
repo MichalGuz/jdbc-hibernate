09b2b36bfeb5ae00521f47cf0d395cbaa4c56a03
MichalGuz <michalguz@gmail.com>
2019-11-28 23:24:49 +0100

    Create fields of class Item.

--- src/main/java/com/spring/jdbchibernate/hibernate_invoice/Item.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_invoice/Item.java
@@ -1,4 +1,11 @@
 package com.spring.jdbchibernate.hibernate_invoice;
 
+import java.math.BigDecimal;
+
 public class Item {
+    int id;
+    Product product;
+    BigDecimal price;
+    int quantity;
+    BigDecimal value;
 }
