8f050874247a5c730ee932a48f28483f7103ee5f
MichalGuz <michalguz@gmail.com>
2019-11-28 23:52:26 +0100

    Add annotations @Repository and @Transactional to nterface ItemDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_invoice/dao/ItemDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_invoice/dao/ItemDao.java
@@ -2,6 +2,11 @@ package com.spring.jdbchibernate.hibernate_invoice.dao;
 
 import com.spring.jdbchibernate.hibernate_invoice.Item;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface ItemDao extends CrudRepository<Item, Integer> {
 }
