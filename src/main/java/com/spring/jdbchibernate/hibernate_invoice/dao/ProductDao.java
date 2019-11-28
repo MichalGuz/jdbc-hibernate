26ea2a4b632fa9de8c30309eddfbe80ac6b91b38
MichalGuz <michalguz@gmail.com>
2019-11-28 23:21:40 +0100

    Interface ProductDao extends CrudRepository<Product, Integer>.

--- src/main/java/com/spring/jdbchibernate/hibernate_invoice/dao/ProductDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_invoice/dao/ProductDao.java
@@ -1,4 +1,7 @@
 package com.spring.jdbchibernate.hibernate_invoice.dao;
 
-public interface ProductDao {
+import com.spring.jdbchibernate.hibernate_invoice.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductDao extends CrudRepository<Product, Integer> {
 }
