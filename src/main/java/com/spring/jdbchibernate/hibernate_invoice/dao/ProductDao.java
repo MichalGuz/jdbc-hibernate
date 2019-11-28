253c99b485e1aca3fa9d2276ef4e7fea535b0ae5
MichalGuz <michalguz@gmail.com>
2019-11-28 23:22:37 +0100

    Add annotations @Repository and @Transactional to nterface ProductDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_invoice/dao/ProductDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_invoice/dao/ProductDao.java
@@ -2,6 +2,11 @@ package com.spring.jdbchibernate.hibernate_invoice.dao;
 
 import com.spring.jdbchibernate.hibernate_invoice.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface ProductDao extends CrudRepository<Product, Integer> {
 }
