aaf9b579ef131618ec1d7ba15a3335421b41a9ae
MichalGuz <michalguz@gmail.com>
2019-11-28 22:01:30 +0100

    Add arguments into interface CrudeRepository definition in SubscriptionDao interface.

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDao.java
@@ -1,5 +1,6 @@
 package com.spring.jdbchibernate.hibernate_mtm_subscription.dao;
 
+import com.spring.jdbchibernate.hibernate_mtm_subscription.Subscription;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,5 @@ import javax.transaction.Transactional;
 
 @Transactional
 @Repository
-public interface SubscriptionDao extends CrudRepository {
+public interface SubscriptionDao extends CrudRepository<Subscription, Integer> {
 }
--- src/test/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDaoTestSuite.java
@@ -5,6 +5,7 @@ import com.spring.jdbchibernate.hibernate_mtm_subscription.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
