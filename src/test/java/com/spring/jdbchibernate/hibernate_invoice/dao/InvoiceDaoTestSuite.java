77222f4049b83ad14a265f4881a797e99d92bbea
MichalGuz <michalguz@gmail.com>
2019-11-30 00:32:03 +0100

    Create new Item objects in 'given' section of testInvoiceDaoSave().

--- src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
@@ -1,11 +1,14 @@
 package com.spring.jdbchibernate.hibernate_invoice.dao;
 
 
+import com.spring.jdbchibernate.hibernate_invoice.Item;
 import com.spring.jdbchibernate.hibernate_invoice.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,6 +24,12 @@ public class InvoiceDaoTestSuite {
         Product product2 = new Product("cola");
         Product product3 = new Product("tonic");
 
+        Item item1 = new Item(new BigDecimal("4"), 1000, new BigDecimal("4000"));
+        Item item2 = new Item(new BigDecimal("6"), 500, new BigDecimal("3000"));
+        Item item3 = new Item(new BigDecimal("5"), 500, new BigDecimal("2500"));
+        Item item4 = new Item(new BigDecimal("4"), 5000, new BigDecimal("10000"));
+        Item item5 = new Item(new BigDecimal("6"), 1000, new BigDecimal("6000"));
+        Item item6 = new Item(new BigDecimal("5"), 200, new BigDecimal("1000"));
 
     }
 }
