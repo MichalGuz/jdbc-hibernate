f93885006d6d7f9aa85cd6249693a1b25be79c73
MichalGuz <michalguz@gmail.com>
2019-11-30 00:17:34 +0100

    Create testInvoiceDaoSave() in class InvoiceDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
@@ -1,6 +1,7 @@
 package com.spring.jdbchibernate.hibernate_invoice.dao;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,4 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InvoiceDaoTestSuite {
+    @Test
+    public void testInvoiceDaoSave() {
+
+    }
 }
