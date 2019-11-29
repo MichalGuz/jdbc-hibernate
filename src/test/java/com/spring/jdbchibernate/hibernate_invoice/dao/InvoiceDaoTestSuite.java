47c28a5925b8c769e09a955dc4c63c64c987207b
MichalGuz <michalguz@gmail.com>
2019-11-30 00:19:15 +0100

    Create fields ProductDao, ItemDao and InvoiceDao in class InvoiceDaoTestSuite.

--- src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
@@ -9,6 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InvoiceDaoTestSuite {
+    ProductDao productDao;
+    ItemDao itemDao;
+    InvoiceDao invoiceDao;
+
     @Test
     public void testInvoiceDaoSave() {
 
