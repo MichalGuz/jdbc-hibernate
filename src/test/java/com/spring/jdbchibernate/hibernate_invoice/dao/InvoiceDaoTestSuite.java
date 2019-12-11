82571e2558bd7e0704c75f4d93472f43bb6d5eff
MichalGuz <michalguz@gmail.com>
2019-12-12 00:07:53 +0100

    Delete rows of code with comments.

--- src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
+++ src/test/java/com/spring/jdbchibernate/hibernate_invoice/dao/InvoiceDaoTestSuite.java
@@ -29,25 +29,21 @@ public class InvoiceDaoTestSuite {
         // given
         Product product1 = new Product("water");
         Product product2 = new Product("cola");
-//
         Product product3 = new Product("tonic");
 
         Item item1 = new Item(new BigDecimal("4"), 1000, new BigDecimal("4000"));
         Item item2 = new Item(new BigDecimal("6"), 500, new BigDecimal("3000"));
-//
         Item item3 = new Item(new BigDecimal("5"), 500, new BigDecimal("2500"));
         Item item4 = new Item(new BigDecimal("4"), 5000, new BigDecimal("10000"));
         Item item5 = new Item(new BigDecimal("6"), 1000, new BigDecimal("6000"));
         Item item6 = new Item(new BigDecimal("5"), 200, new BigDecimal("1000"));
 
         Invoice invoice1 = new Invoice("FV 01/01/2019");
-//
         Invoice invoice2 = new Invoice("FV 01/02/2019");
         Invoice invoice3 = new Invoice("FV 01/03/2019");
 
         invoice1.getItems().add(item1);
         invoice1.getItems().add(item2);
-//
         invoice2.getItems().add(item3);
         invoice2.getItems().add(item4);
         invoice3.getItems().add(item5);
@@ -55,7 +51,6 @@ public class InvoiceDaoTestSuite {
 
         product1.getItems().add(item1);
         product2.getItems().add(item2);
-//
         product3.getItems().add(item3);
         product1.getItems().add(item4);
         product2.getItems().add(item5);
@@ -63,7 +58,6 @@ public class InvoiceDaoTestSuite {
 
         item1.setProduct(product1);
         item2.setProduct(product2);
-//
         item3.setProduct(product3);
         item4.setProduct(product1);
         item5.setProduct(product2);
@@ -71,7 +65,6 @@ public class InvoiceDaoTestSuite {
 
         item1.setInvoice(invoice1);
         item2.setInvoice(invoice1);
-//
         item3.setInvoice(invoice2);
         item4.setInvoice(invoice2);
         item5.setInvoice(invoice3);
@@ -79,18 +72,15 @@ public class InvoiceDaoTestSuite {
 
         // when
         invoiceDao.save(invoice1);
-//
         invoiceDao.save(invoice2);
         invoiceDao.save(invoice3);
 
         productDao.save(product1);
         productDao.save(product2);
-//
         productDao.save(product3);
 
         itemDao.save(item1);
         itemDao.save(item2);
-//
         itemDao.save(item3);
         itemDao.save(item4);
         itemDao.save(item5);
@@ -98,24 +88,15 @@ public class InvoiceDaoTestSuite {
 
 
         int invoiceId1 = invoice1.getId();
-//
         int invoiceId2 = invoice2.getId();
         int invoiceId3 = invoice3.getId();
 
         // then
         Assert.assertNotEquals(0, invoiceId1);
-//
         Assert.assertNotEquals(0, invoiceId2);
         Assert.assertNotEquals(0, invoiceId3);
 
         // cleanUp
-//        itemDao.delete(item1);
-//        itemDao.delete(item2);
-//        itemDao.delete(item3);
-//        itemDao.delete(item4);
-//        itemDao.delete(item5);
-//        itemDao.delete(item6);
-
         itemDao.deleteById(item1.getId());
         itemDao.deleteById(item2.getId());
         itemDao.deleteById(item3.getId());
@@ -129,10 +110,5 @@ public class InvoiceDaoTestSuite {
         productDao.deleteById(product3.getId());
 
         invoiceDao.deleteAll();
-
-//        productDao.deleteAll();
-//        invoiceDao.deleteAll();
-//        itemDao.deleteAll();
-//
        }
 }
