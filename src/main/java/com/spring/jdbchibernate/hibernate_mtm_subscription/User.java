e4adab6e0643f03e2a018ce60c33d24a168758b7
MichalGuz <michalguz@gmail.com>
2019-12-15 15:54:45 +0100

    Correct a typo in retrieveUserWithLastnameEquals().

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/User.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/User.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @NamedQuery(
-        name = "User.RetrieveUserWithLastnameEquals",
+        name = "User.retrieveUserWithLastnameEquals",
         query = "FROM User WHERE lastname = :LASTNAME"
 )
 @Entity
