253f9e28ca1da63de2216cef39cb87beb45e0478
MichalGuz <michalguz@gmail.com>
2019-12-15 16:06:36 +0100

    Create @Query with retrieveUserWithLastNameEquals() method and parameter @Param LASTNAME in the interface SubscriptionDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/SubscriptionDao.java
@@ -1,12 +1,18 @@
 package com.spring.jdbchibernate.hibernate_mtm_subscription.dao;
 
 import com.spring.jdbchibernate.hibernate_mtm_subscription.Subscription;
+import com.spring.jdbchibernate.hibernate_mtm_subscription.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
 public interface SubscriptionDao extends CrudRepository<Subscription, Integer> {
+    @Query
+    List<User> retrieveUserWithLastNameEquals(@Param("LASTNAME") String lastName);
 }
