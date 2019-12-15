66beac97e49cc497f9bb2beb1ced01adf3fe7211
MichalGuz <michalguz@gmail.com>
2019-12-15 16:03:11 +0100

    Correct the name of method in @NamedQuery for User entity.

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/User.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/User.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @NamedQuery(
-        name = "User.retrieveUserWithLastnameEquals",
+        name = "User.retrieveUserWithLastNameEquals",
         query = "FROM User WHERE lastName = :LASTNAME"
 )
 @Entity
