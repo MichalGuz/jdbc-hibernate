33154b94430d8a81ed0b8fb2adc8aff95158acc3
MichalGuz <michalguz@gmail.com>
2019-12-15 23:41:37 +0100

    Add annotations @Transactional and @Repository to the interface UserDao.

--- src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/UserDao.java
+++ src/main/java/com/spring/jdbchibernate/hibernate_mtm_subscription/dao/UserDao.java
@@ -4,9 +4,13 @@ import com.spring.jdbchibernate.hibernate_mtm_subscription.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
+@Repository
 public interface UserDao extends CrudRepository<User, Integer> {
     @Query
     List<User> retrieveUserWithLastNameEquals(@Param("LASTNAME") String lastName);
